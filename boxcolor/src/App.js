5d47df5d6c24d6baa5f56fecb079b43230eb1452
Student_M20 <student_m20@PS-DAL-M20.local>
2020-03-25 19:30:51 -0500

    solution to toggle between original color and random color

--- boxcolor/src/App.js
+++ boxcolor/src/App.js
@@ -6,16 +6,29 @@ export default class ComponentName extends Component{
     super(props);
     this.state={
       color:'blue',
+      originalColor:'orange',
       clickCounter:0
     }
   }
   onChange=(e)=>{
-    let color=(this.state.color==='blue')?'purple':'blue';
-    this.setState({
-      color:color,
-      clickCounter:this.state.clickCounter+1
-    })
+      let color='';
+      let counter = this.state.clickCounter;
+      //console.log("counter",counter);
+      //toggles color between default color and random color
+      if(counter%2 !=0){
+          color=this.state.originalColor;
+      }
+      else{
+            color = "#"+Math.floor(Math.random()*16777215).toString(16);
+           // console.log("color",color);
+          }
+      this.setState({
+          color:color,
+          clickCounter:this.state.clickCounter+1
+        })
     console.log(this.state.clickCounter);
+    this.state.originalColor = this.state.color;
+
   }
   render(){
     return(
